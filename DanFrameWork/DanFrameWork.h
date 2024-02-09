5305ceab09ed73ef32bd82571174afa2588acac3
MacBookPro <kimani.daniel@eclectics.io>
2024-02-09 21:23:45 +0300

    Initial Commit

---
+++ DanFrameWork/DanFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  DanFrameWork.h
+//  DanFrameWork
+//
+//  Created by Daniel Kimani on 09/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DanFrameWork.
+FOUNDATION_EXPORT double DanFrameWorkVersionNumber;
+
+//! Project version string for DanFrameWork.
+FOUNDATION_EXPORT const unsigned char DanFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DanFrameWork/PublicHeader.h>
+
+
