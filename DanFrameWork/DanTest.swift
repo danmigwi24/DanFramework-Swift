6aa45f86dd5d2226821fbbe88b82c8bd0680f437
MacBookPro <kimani.daniel@eclectics.io>
2024-02-20 09:09:12 +0300

    first commit

---
+++ DanFrameWork/DanTest.swift
@@ -0,0 +1,19 @@
+//
+//  DanTest.swift
+//  DanFrameWork
+//
+//  Created by Daniel Kimani on 09/02/2024.
+//
+
+import Foundation
+
+public class DanTest {
+    public init() {
+        
+    }
+    
+    public func printLogger(_ message:String){
+        print(message)
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# DanFramework-Swift
